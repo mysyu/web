fb458c459ed317aab217d90fc13e92572719b505
j4682739150 <j4682739150@yahoo.com.tw>
2017-10-13 22:48:11 +0800

    ok

---
+++ Web/DB.cs
@@ -0,0 +1,79 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Web
+{
+    public class DB
+    {
+        private static String DBHost = "mysyu.ddns.net";
+        private static String DBUser = "mysyu";
+        private static String DBPassword = "mysyu";
+        private static String DBName = "mysyu";
+        private static String DBCharSet = "utf8";
+        private static String DBTimeout = "1";
+        private static MySqlConnection DBConnection = null;
+
+        public DB()
+        {
+            Connect();
+        }
+        public DataTable Select(String command,Dictionary<String,Object>dic)
+        {
+            Connect();
+            DataTable result = new DataTable();
+            MySqlCommand cmd = new MySqlCommand(command, DBConnection);
+            foreach (var key in dic.Keys)
+                cmd.Parameters.AddWithValue(key, dic[key]);
+            result.Load(cmd.ExecuteReader());
+            DisConnect();
+            return result;
+        }
+        public int ExecuteNonQuery(String command, Dictionary<String, Object> dic)
+        {
+            Connect();
+            MySqlCommand cmd = new MySqlCommand(command, DBConnection);
+            foreach (var key in dic.Keys)
+                cmd.Parameters.AddWithValue(key, dic[key]);
+            int result = cmd.ExecuteNonQuery();
+            DisConnect();
+            return result;
+        }
+        public Object ExecuteScalar(String command, Dictionary<String, Object> dic)
+        {
+            Connect();
+            MySqlCommand cmd = new MySqlCommand(command, DBConnection);
+            foreach (var key in dic.Keys)
+                cmd.Parameters.AddWithValue(key, dic[key]);
+            Object result = cmd.ExecuteScalar();
+            DisConnect();
+            return result;
+        }
+        public bool isConnect
+        {
+            get
+            {
+                return DBConnection != null;
+            }
+        }
+        public void Connect()
+        {
+            if (!isConnect)
+            {
+                DBConnection = new MySqlConnection(String.Format("server={0};uid={1};pwd={2};database={3};charset={4};Connection Timeout={5}", DBHost, DBUser, DBPassword, DBName, DBCharSet, DBTimeout));
+                DBConnection.Open();
+            }
+        }
+        public void DisConnect()
+        {
+            if (isConnect)
+            {
+                DBConnection.Close();
+                DBConnection = null;
+            }
+        }
+    }
+}
--- Web/default.aspx
+++ Web/default.aspx
@@ -4,13 +4,16 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script>
+        $(window).load(function () {
+        });
+    </script>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-        </div>
     </form>
 </body>
 </html>
